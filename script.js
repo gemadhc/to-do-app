cd891dece80e9393d7b3d8a8c5044b48987cea48
gemadhc <39023501+gemadhc@users.noreply.github.com>
2022-10-09 17:05:59 -0700

    Read input from input bar and prints it to the console

---
+++ script.js
@@ -0,0 +1,9 @@
+var get_text= document.querySelector(".get-task");
+
+get_text.onkeyup = function(){
+  console.log(get_text.value);
+}
+
+
+// next task is to get the text to show up in the list
+//create a new li element and set the inner html to whatever is typed 
