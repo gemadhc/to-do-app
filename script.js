3f056d87c493599e38ca1e2809fbac93dde9906a
gemadhc <39023501+gemadhc@users.noreply.github.com>
2022-10-19 19:52:53 -0700

    Script for text to show in the list
    Added script for text to show up in the list after being typed in the input text  box and clicking on the "add new item" button

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 var get_text= document.querySelector(".get-task");
+var string = "";
 
 get_text.onkeyup = function(){
-  console.log(get_text.value);
+  //save string in a variable
+  string = get_text.value;
 }
 
 
+function show_text(){
+  console.log(string);
+
+  //append new task to list
+  var list = document.querySelector(".task-list");
+  var new_task = document.createElement("li");
+  new_task.innerHTML = string;
+  list.append(new_task);
+  string ="";
+  get_text.value = ""
+}
+
+document.querySelector(".add-button").addEventListener("click", show_text);
+
 // next task is to get the text to show up in the list
-//create a new li element and set the inner html to whatever is typed 
+//create a new li element and set the inner html to whatever is typed
