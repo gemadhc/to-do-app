b9e0cf27a3615547e13fed2f00f77288ec865117
gemadhc <39023501+gemadhc@users.noreply.github.com>
2022-11-03 18:58:29 -0700

    check box function
    the task disappears when the checkbox is clicked

--- script.js
+++ script.js
@@ -1,33 +1,54 @@
 var get_text= document.querySelector(".get-task");
 var string = "";
-
+var task_total = 0;
 get_text.onkeyup = function(){
   //save string in a variable
   string = get_text.value;
+};
+
+function disappear_task(checkedid){
+  if (checkedid.checked == true){
+
+    var new_id = ".id"+checkedid.id;
+    document.querySelector(new_id).remove();
+    checkedid.remove();
+    task_total = task_total -1;
+  }
 }
 
-
 function show_text(){
-  console.log(string);
+  if(string != ""){
+    task_total = task_total  + 1;
+    console.log(string);
+    //append new task to list
+    var list = document.querySelector(".task-list");
+    var list_item = document.createElement("li");
+    var new_task = document.createElement("input");
+    var task_name = document.createElement("label");
+    task_name.innerHTML = string;
+    task_name.classList.add("task-name");
+    var counter = "" + task_total +"";
+    task_name.setAttribute("for", counter);
+    var counter_id = "id" + counter;
+    task_name.classList.add(counter_id);
+    new_task.setAttribute("id", counter);
+    new_task.setAttribute("type", "checkbox");
+    new_task.setAttribute("onclick", "disappear_task(this)");
+    new_task.classList.add("check");
 
-  //append new task to list
-  var list = document.querySelector(".task-list");
-  var list_item = document.createElement("li");
-  var new_task = document.createElement("input");
-  var task_name = document.createElement("h4");
-  task_name.innerHTML = string;
-  task_name.classList.add("task-name");
-  new_task.setAttribute("type", "checkbox");
-  new_task.classList.add("check");
-
-  list_item.append(new_task);
-  list_item.append(task_name);
-  list.append(list_item);
-  string ="";
-  get_text.value = ""
+    list_item.append(new_task);
+    list_item.append(task_name);
+    list.append(list_item);
+    string ="";
+    get_text.value = ""
+  }
 }
 
 document.querySelector(".add-button").addEventListener("click", show_text);
-
+document.querySelector(".get-task").addEventListener("keypress", function(event){
+    if(event.key=="Enter"){
+      show_text();
+    }
+});
 // next task is to get the text to show up in the list
 //create a new li element and set the inner html to whatever is typed
